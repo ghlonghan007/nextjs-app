6bfc8fc22a2d1f5c1d869a8f9956ab8800f8da34
ghlonghan007 <861152110@qq.com>
2024-06-10 02:45:57 +0800

    登入注册

--- web/app/lib/actions.ts
+++ web/app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod'
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { signIn } from '@/auth'
+import { AuthError } from 'next-auth'
 
 const FormSchema = z.object({
   id: z.string(),
@@ -72,18 +74,24 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
 // ...
 
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData
+) {
   const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   })
+
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing Fields. Failed to Create Invoice.',
+      message: 'Missing Fields. Failed to Update Invoice.',
     }
   }
+  const { customerId, amount, status } = validatedFields.data
   const amountInCents = amount * 100
   try {
     await sql`
@@ -100,11 +108,29 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice')
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`
     revalidatePath('/dashboard/invoices')
   } catch (error) {
     return { message: 'Database Error: Failed to Delete Invoice.' }
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn('credentials', formData)
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.'
+        default:
+          return 'Something went wrong.'
+      }
+    }
+    throw error
   }
 }
